85ebfb203b49775914d8b1472f3f4a61e54b4873
Davide Mendolia <davide@gokarumi.com>
2017-03-13 16:33:55 +0100

    Move InternalsVisibleTo to Assembly

--- KataContactsCSharp/Use Cases/GetContacts.cs
+++ KataContactsCSharp/Use Cases/GetContacts.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
-[assembly: InternalsVisibleTo("KataContactsCSharp.Tests")]
 namespace KataContactsCSharp
 {
 	public class GetContacts
